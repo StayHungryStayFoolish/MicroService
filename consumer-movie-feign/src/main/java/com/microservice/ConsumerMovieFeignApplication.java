ef649710530c2067192008a4c5f29c3d3bd4c408
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-08 15:31:04 +0800

    MicroService 简单实战

--- consumer-movie-feign/src/main/java/com/microservice/ConsumerMovieFeignApplication.java
+++ consumer-movie-feign/src/main/java/com/microservice/ConsumerMovieFeignApplication.java
@@ -2,11 +2,19 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+/**
+ * @EnableDiscoveryClient 向服务中心注册
+ * @EnableFeignClients 开启 Feign 功能
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerMovieFeignApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerMovieFeignApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieFeignApplication.class, args);
+    }
 }
