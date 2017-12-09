70bdb8530dac30183ead7ff42e36815bf15feb70
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-09 12:49:01 +0800

    MicroService 简单实战

--- consumer-movie-feign-hystrix/src/main/java/com/microservice/service/FeignHystrixService.java
+++ consumer-movie-feign-hystrix/src/main/java/com/microservice/service/FeignHystrixService.java
@@ -1,6 +1,7 @@
 package com.microservice.service;
 
 import com.microservice.entity.User;
+import com.microservice.service.impl.FeignHystrixServiceImpl;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by bonismo@hotmail.com
  * 下午12:31 on 17/12/9.
  */
-@FeignClient(name = "provider-user")
+@FeignClient(name = "provider-user",fallback = FeignHystrixServiceImpl.class)
 public interface FeignHystrixService {
 
     @RequestMapping(value = "/{id}")
