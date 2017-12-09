c67cc7df02197e1891cfdbd7618c27e548222a82
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-09 15:02:23 +0800

    MicroService 简单实战

--- hystrix-dashboard/src/main/java/com/microservice/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/microservice/HystrixDashboardApplication.java
@@ -2,7 +2,13 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+/**
+ * @EnableHystrixDashboard
+ * 开启监控器仪表盘
+ */
+@EnableHystrixDashboard
 @SpringBootApplication
 public class HystrixDashboardApplication {
 
