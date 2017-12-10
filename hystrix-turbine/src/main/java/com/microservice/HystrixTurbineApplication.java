0a16de83795bcefcd3b4775b3c792010bb722c21
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-10 20:01:30 +0800

    MicroService 简单实战

--- hystrix-turbine/src/main/java/com/microservice/HystrixTurbineApplication.java
+++ hystrix-turbine/src/main/java/com/microservice/HystrixTurbineApplication.java
@@ -2,7 +2,13 @@ package com.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * @EnbaleTurbine
+ * 开启 Turbine 集群监控服务
+ */
+@EnableTurbine
 @SpringBootApplication
 public class HystrixTurbineApplication {
 
