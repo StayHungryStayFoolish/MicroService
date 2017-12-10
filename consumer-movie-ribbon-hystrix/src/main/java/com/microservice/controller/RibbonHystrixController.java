84027ad5340992f123b5df86a24c95f57cbe217b
StayHungryStayFoolish <bonismo@hotmail.com>
2017-12-10 17:14:05 +0800

    MicroService 简单实战

--- consumer-movie-ribbon-hystrix/src/main/java/com/microservice/controller/RibbonHystrixController.java
+++ consumer-movie-ribbon-hystrix/src/main/java/com/microservice/controller/RibbonHystrixController.java
@@ -3,6 +3,8 @@ package com.microservice.controller;
 import com.microservice.entity.User;
 import com.microservice.service.RibbonHystrixService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,22 @@ public class RibbonHystrixController {
     @Autowired
     private RibbonHystrixService ribbonHystrixService;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+
     @GetMapping(value = "/ribbon/{id}")
     public User findById(@PathVariable Long id) {
         return ribbonHystrixService.findById(id);
+    }
+
+    /**
+     * 本地服务实例的信息
+     * @return
+     */
+    @GetMapping("/instance-info")
+    public ServiceInstance showInfo() {
+        ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+        return localServiceInstance;
     }
 }
---
+++ hystrix-turbine/src/main/java/com/microservice/HystrixTurbineApplication.java
@@ -0,0 +1,12 @@
+package com.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixTurbineApplication.class, args);
+	}
+}
